3a82ec2957072a7d221f28a10c8325e8c1ab33b5
vdegenne <vdegenne@gmail.com>
2023-06-17 19:13:45 +0200

    move dotenv at the beginning of config file

--- src/config.ts
+++ src/config.ts
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import {config} from 'dotenv';
+config();
 
 export default {
   stripeSecretKey: process.env.STRIPE_API_KEY,
